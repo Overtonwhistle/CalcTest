93b3e366832af5b50dcbd49c794dcce924b63ff7
Pavel Sarakaletau <pavel_sarakaletau@epam.com>
2018-10-02 22:31:10 +0300

    @BefoteClass in Base

--- src/test/java/com/epam/tat/calctest/testng/BaseTest.java
+++ src/test/java/com/epam/tat/calctest/testng/BaseTest.java
@@ -1,8 +1,8 @@
 package com.epam.tat.calctest.testng;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 import com.epam.tat.module4.Calculator;
 
@@ -14,13 +14,13 @@ public class BaseTest {
 
 	protected Calculator calculator;
 
-	@BeforeTest(alwaysRun = true)
+	@BeforeClass(alwaysRun = true)
 	public void setUp() {
 		calculator = new Calculator();
 		Assert.assertTrue(calculator != null, "Calculator object not created!");
 	}
 
-	@AfterTest
+	@AfterClass
 	public void tearDown() {
 		calculator = null;
 	}
